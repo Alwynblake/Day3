0d404195cf9efd9be9270885736fdd05d31dcb5d
Alistair Blake <consultations@me.com>
2018-10-24 19:06:01 -0700

    updated again!

--- app.js
+++ app.js
@@ -1,50 +1,77 @@
-"use strict"; 
-console.log('checking user userName before prompt', userName);
-var userName = prompt('What is your name?');
-alert('Thanks for visiting my site ' + userName);
-console.log('checking userName after prompt', userName);
+"use strict";
+// console.log('checking user userName before prompt', userName);
+// var userName = prompt('What is your name?');
+// alert('Thanks for visiting my site ' + userName);
+// console.log('checking userName after prompt', userName);
 
-alert('Please respond to the following questions with Y/y/yes/YES or no...');
+// alert('Please respond to the following questions with Y/y/yes/YES or no...');
 
-var likeDogs = prompt('Do I like dogs?').toUpperCase();
-console.log ('checking userName: ', userName);
-if (likeDogs === 'Y' || likeDogs === 'YES') {
-    alert('Congrats! ' + userName + ', you know me extremely well.')
+// var likeDogs = prompt('Do I like dogs?').toUpperCase();
+// console.log ('checking userName: ', userName);
+//     if (likeDogs === 'Y' || likeDogs === 'YES') {
+//         alert('Congrats! ' + userName + ', you know me extremely well.')
+//         } else {
+//         alert('Sorry, ' + userName + ' you were wrong.');
+// }
+
+// var likeLearning = prompt('Do I like to learn?').toUpperCase();
+// console.log ('Do I like learning?', likeLearning);
+// if (likeLearning === 'Y' || likeLearning === 'YES') {
+//     alert('Congrats! ' + userName + ', you know me extremely well.')
+// }else {
+//     alert('Sorry, ' + userName + ' you were wrong.')
+
+// }
+
+//      var likeMovies = prompt('Do I like movies?').toUpperCase();
+//      console.log ('Do I like movies?', likeMovies);  
+// if (likeMovies === 'Y' || likeMovies === 'YES') {
+//     alert('Congrats! ' + userName + ', you know me extremely well.')
+// }else {
+//     alert('Sorry, ' + userName + ' you were wrong.')
+
+// }   
+
+//     var likeFood = prompt('Do I like to eat good food?').toUpperCase();
+//     console.log ('Do I like food?', likeFood);  
+// if (likeFood === 'Y' || likeFood === 'YES') {
+//     alert('Congrats! ' + userName + ', you know me extremely well.')
+// }else {
+//     alert('Sorry, ' + userName + ' you were wrong.')
+// }
+
+//     var likeIcecream = prompt('Do I like to ice cream?').toUpperCase();
+//     console.log ('Do I like ice cream?', likeIcecream);  
+// if (likeIcecream === 'Y' || likeIcecream=== 'YES') {
+//     alert('Congrats! ' + userName + ', you know me extremely well.')
+// }else {
+//     alert('Sorry, ' + userName + ' you were wrong.')
+// }
+
+var favNumber = 32
+var counter = 0;
+var numberGuess = parseInt(prompt('guess my favorite number. you have four chances!'));
+
+while (numberGuess !== favNumber && counter < 3) {
+  console.log(counter);
+  counter++;
+
+  if (numberGuess < favNumber) {
+
+    numberGuess = parseInt(prompt('Your guess was too low. Try again'))
+  } else if (numberGuess > favNumber) {
+    numberGuess = parseInt(prompt('Your guess was too high. Try again.'));
+  }
+}
+if (numberGuess === favNumber) { 
+  alert('correct'); 
 } else {
-alert('Sorry, ' + userName + ' you were wrong.')
+  alert('The number is ' + favNumber);
 
-
-}
-var likeLearning = prompt('Do I like to learn?').toUpperCase();
-console.log ('Do I like learning?', likeLearning);
-if (likeLearning === 'Y' || likeLearning === 'YES') {
-    alert('Congrats! ' + userName + ', you know me extremely well.')
-}else {
-    alert('Sorry, ' + userName + ' you were wrong.')
+//create an array
+var guessPlaces = prompt('Can you guess a state that I have lived in besides Washington?');
+var placesLived = ['New York', 'Dallas', 'Massachusetts']
+console.log(placesLived);
 
 }
 
-     var likeMovies = prompt('Do I like movies?').toUpperCase();
-     console.log ('Do I like movies?', likeMovies);  
-if (likeMovies === 'Y' || likeMovies === 'YES') {
-    alert('Congrats! ' + userName + ', you know me extremely well.')
-}else {
-    alert('Sorry, ' + userName + ' you were wrong.')
-
-}   
-
-    var likeFood = prompt('Do I like to eat good food?').toUpperCase();
-    console.log ('Do I like food?', likeFood);  
-if (likeFood === 'Y' || likeFood === 'YES') {
-    alert('Congrats! ' + userName + ', you know me extremely well.')
-}else {
-    alert('Sorry, ' + userName + ' you were wrong.')
-}
-
-    var likeIcecream = prompt('Do I like to ice cream?').toUpperCase();
-    console.log ('Do I like ice cream?', likeIcecream);  
-if (likeIcecream === 'Y' || likeIcecream=== 'YES') {
-    alert('Congrats! ' + userName + ', you know me extremely well.')
-}else {
-    alert('Sorry, ' + userName + ' you were wrong.')
-}
